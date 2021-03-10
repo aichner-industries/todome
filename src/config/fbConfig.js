7e2ef039278fe0b3667dafe8d51865bcb70119e0
Christian Aichner <contact@aichner-christian.com>
2021-03-10 18:40:27 +0100

    Add files
    Hidden files have been added.

---
+++ src/config/fbConfig.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFdvRAm8V6K9zeR14_q0qEc_YJ_jGjFgM",
+  authDomain: "todome-ai.firebaseapp.com",
+  projectId: "todome-ai",
+  storageBucket: "todome-ai.appspot.com",
+  messagingSenderId: "1025197690875",
+  appId: "1:1025197690875:web:33ffd21008012ff235855e",
+};
+
+export default firebaseConfig;
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2021 Christian Aichner
+ */
